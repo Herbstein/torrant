45aeaf48080828146a0e48bc19e45bcd126a7a06
Rasmus Larsen <doctorherbstein@gmail.com>
2021-09-29 22:48:02 +0200

    Rename test torrent to clarify content

--- examples/read_torrent.rs
+++ examples/read_torrent.rs
@@ -1,7 +1,7 @@
 use torrant::metainfo::Metainfo;
 
 fn main() {
-    let x = std::fs::read("data/test.torrent").unwrap();
+    let x = std::fs::read("data/ubuntu.torrent").unwrap();
     let info = serde_bencode::from_bytes::<Metainfo>(&x).unwrap();
 
     println!("{:#?}", info);
