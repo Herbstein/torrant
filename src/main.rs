910d6eb964ae7abfcdc19db9982eb331a282ae57
Rasmus Larsen <doctorherbstein@gmail.com>
2023-08-30 14:27:03 +0200

    Hacky tracker-less download

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::Ipv4Addr;
+use std::{net::Ipv4Addr, time::Duration};
 
 use anyhow::Result;
 use futures::{SinkExt, StreamExt};
@@ -63,32 +63,32 @@ async fn main() -> Result<()> {
 
     let client = Client::new();
 
-    let mut req = client.get(torrent.announce()).build()?;
-    req.url_mut().set_query(Some(&format!(
-       "info_hash={info_hash_formencoded}&peer_id={peer_id_formencoded}&port=6881&uploaded=0&downloaded=0&left={left}&event=started&compact=1"
-    )));
+    // let mut req = client.get(torrent.announce()).build()?;
+    // req.url_mut().set_query(Some(&format!(
+    //    "info_hash={info_hash_formencoded}&peer_id={peer_id_formencoded}&port=6881&uploaded=0&downloaded=0&left={left}&event=started&compact=1"
+    // )));
 
-    let resp = client.execute(req).await?;
-    let body = resp.bytes().await?;
+    // let resp = client.execute(req).await?;
+    // let body = resp.bytes().await?;
 
-    let tracker_response = bendy::serde::from_bytes::<CompactTrackerResponse>(&body)?;
-    assert!(tracker_response.peers.len() % 6 == 0);
+    // let tracker_response = bendy::serde::from_bytes::<CompactTrackerResponse>(&body)?;
+    // assert!(tracker_response.peers.len() % 6 == 0);
 
-    let peers = tracker_response
-        .peers
-        .chunks_exact(6)
-        .map(|x| {
-            let mut ip = [0; 4];
-            ip.copy_from_slice(&x[..4]);
-            let ip = Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]);
+    // let peers = tracker_response
+    //     .peers
+    //     .chunks_exact(6)
+    //     .map(|x| {
+    //         let mut ip = [0; 4];
+    //         ip.copy_from_slice(&x[..4]);
+    //         let ip = Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]);
 
-            let mut port = [0; 2];
-            port.copy_from_slice(&x[4..]);
-            let port = u16::from_be_bytes(port);
+    //         let mut port = [0; 2];
+    //         port.copy_from_slice(&x[4..]);
+    //         let port = u16::from_be_bytes(port);
 
-            (ip, port)
-        })
-        .collect::<Vec<_>>();
+    //         (ip, port)
+    //     })
+    //     .collect::<Vec<_>>();
     // println!("{:?}", peers);
 
     let mut buffer = vec![0; torrent.info.length()];
@@ -97,12 +97,14 @@ async fn main() -> Result<()> {
     let (mut writer, mut reader) = framed.split();
 
     writer.send(PeerMessage::Interested).await?;
-    writer.send(PeerMessage::Unchoke).await?;
+    // writer.send(PeerMessage::Unchoke).await?;
 
     let mut current_piece = 0;
 
     while let Some(Ok(data)) = reader.next().await {
         // println!("{data:x?}");
+
+        let total_full_pieces = torrent.info.length() / torrent.info.piece_length();
 
         match data {
             PeerMessage::Unchoke => {
@@ -125,8 +127,6 @@ async fn main() -> Result<()> {
 
                 current_piece += 1;
 
-                let total_full_pieces = torrent.info.length() / torrent.info.piece_length();
-
                 if current_piece < total_full_pieces as u32 {
                     writer
                         .send(PeerMessage::Request(
@@ -144,7 +144,7 @@ async fn main() -> Result<()> {
                         ))
                         .await?;
                 } else {
-                    println!("Received all bytes!")
+                    println!("Received all bytes!");
                 }
             }
             _ => {}
